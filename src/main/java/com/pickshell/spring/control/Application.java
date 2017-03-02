01062f4538fb705c71ad06e31854f57a197710c8
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-03-02 15:51:07 +0800

    both command line and tomcat

--- src/main/java/com/pickshell/spring/control/Application.java
+++ src/main/java/com/pickshell/spring/control/Application.java
@@ -1,13 +1,18 @@
 package com.pickshell.spring.control;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class Application {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 }

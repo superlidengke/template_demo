f7c2020cfd82f65b231168a8807740dbc3455ccd
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-03-29 18:07:05 +0800

    add logout

--- src/main/java/com/pickshell/security/controller/LoginController.java
+++ src/main/java/com/pickshell/security/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.pickshell.security.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,5 +16,14 @@ public class LoginController {
 	@RequestMapping(method = RequestMethod.GET, value = "/custom_login")
 	public String showLogin() {
 		return "login";
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/custom_login?logout";
 	}
 }

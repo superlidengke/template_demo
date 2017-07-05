e3399e97c80ee9a0396400394298613383214dc6
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-07-05 16:38:01 +0800

    sql defined in mapper.xml

--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
+v0.2
+sql in mapper.xml
+
 v0.1
 xml for session factory
 sql in dao interface
--- src/main/java/com/pickshell/mybatis/dao/UserDao.java
+++ src/main/java/com/pickshell/mybatis/dao/UserDao.java
@@ -1,12 +1,8 @@
 package com.pickshell.mybatis.dao;
 
-import org.apache.ibatis.annotations.Select;
-
 import com.pickshell.domain.User;
 
 public interface UserDao {
-	String MQL_GET_USER_BY_ID = "select * from users where user_id = #{id}";
 
-	@Select(MQL_GET_USER_BY_ID)
 	public User getUserById(String id);
 }

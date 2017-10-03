8f4a8d62a576fc92327b397f600a3cd03b94f1e7
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-10-03 17:57:58 +0800

    quck start,service task in java

---
+++ src/main/java/com/pickshell/app/AutomatedDataDelegate.java
@@ -0,0 +1,17 @@
+package com.pickshell.app;
+
+import java.util.Date;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class AutomatedDataDelegate implements JavaDelegate {
+
+  @Override
+	public void execute(DelegateExecution execution) {
+		Date now = new Date();
+		execution.setVariable("autoWelcomeTime", now);
+		System.out.println("Faux call to backend for [" + execution.getVariable("fullName") + "]");
+  }
+
+}

ab75fe6cafb028eb1b5cdbe6930355c51c680c55
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-10-13 09:36:14 +0800

    add jetty plugin

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Maven web template
+Java restful api
+
+1.run this project by :mvn jetty:run or eclipse jetty plugin 
+2.http get:http://localhost:8080/rest/hello/param
+3.http post:http://localhost:8080/rest/messageService/search,request template body in request class 
---
+++ src/test/java/template_demo/AppTest.java
@@ -0,0 +1,22 @@
+package template_demo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+
+	}
+
+}

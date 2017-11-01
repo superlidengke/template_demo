8445a0e09facdcf29f183b3affde96d260085c03
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-11-01 10:17:45 +0800

    mix java and xml config

--- src/main/java/com/pickshell/CDPlayerConfig.java
+++ src/main/java/com/pickshell/CDPlayerConfig.java
@@ -2,7 +2,10 @@ package com.pickshell;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
 @Configuration
 @ComponentScan //scan bean in the same package or sub
+@ImportResource("classpath:spring_bean.xml")
 public class CDPlayerConfig {
 }
--- src/test/groovy/springdi/SpringBeanTest.groovy
+++ src/test/groovy/springdi/SpringBeanTest.groovy
@@ -5,7 +5,9 @@ import com.pickshell.springdi.service.CDPlayer;
 import com.pickshell.springdi.service.MediaPlayer
 import com.pickshell.springdi.service.Mp4Player;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+import javax.sql.DataSource;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -32,6 +34,9 @@ public class SpringBeanTest {
             CDPlayer cdPlayer = ctx.getBean(CDPlayer.class)
             assert cdPlayer
             cdPlayer.play()
+
+            DataSource dataSource = ctx.getBean("dataSource");
+            assert dataSource
         }catch (Exception e){
             e.printStackTrace()
         }

80b9d5965f6912fd898ee07cc8456ec70754485e
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-11-03 10:10:43 +0800

    execute sql before test method

--- src/test/groovy/com/pickshell/spring/service/UserServiceTest.groovy
+++ src/test/groovy/com/pickshell/spring/service/UserServiceTest.groovy
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
 @ActiveProfiles("prod")
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:test-data.sql")
 class UserServiceTest {
     @Autowired
     UserService service;

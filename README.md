eca9446af079ae345ff4563713d43616b8ebcd7a
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-11-16 17:37:26 +0800

    spring mvn hello world

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+## 2017-11-16
+1.Spring MVC , local test : gradle apprun
 ## 2017-11-06
 1.add spring @Sql in unit test
---
+++ src/main/java/com/pickshell/spring/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.pickshell.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by uc212310 on 11/16/2017.
+ */
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+}

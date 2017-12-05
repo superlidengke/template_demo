31cea0778cb7d60469c4a0d3035c17594870be00
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-12-05 14:08:24 +0800

    create mock object by annotation

--- src/test/groovy/com/pickshell/mock/AuthenticatorApplicationTest.groovy
+++ src/test/groovy/com/pickshell/mock/AuthenticatorApplicationTest.groovy
@@ -3,36 +3,30 @@ package com.pickshell.mock
 import com.pickshell.mock.service.AuthenticatorApplication
 import com.pickshell.mock.service.AuthenticatorInterface
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.runners.MockitoJUnitRunner
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 /**
  * Created by uc212310 on 12/5/2017.
  */
+@RunWith(MockitoJUnitRunner.class)
 class AuthenticatorApplicationTest {
+    @Mock
+    private AuthenticatorInterface authenticatorMock;
+    @InjectMocks
+    private AuthenticatorApplication authenticator;
     @Test
-    public void testAuthenticate() {
-        AuthenticatorInterface authenticatorMock;
-        AuthenticatorApplication authenticator;
-        String username = "JavaCodeGeeks";
-        String password = "unsafePassword";
-        authenticatorMock = Mockito.mock(AuthenticatorInterface.class);
-        authenticator = new AuthenticatorApplication(authenticatorMock);
-        when(authenticatorMock.authenticateUser(username, password))
-                .thenReturn(false);
-        verify(authenticatorMock, never()).authenticateUser(username, password);
-        boolean actual = authenticator.authenticate(username, password);
-        assertFalse(actual);
-        //verify the method of the mock object has been called by the expected arguments
-        verify(authenticatorMock).authenticateUser(username, password);
-        //Verify that method has been called n times
-        verify(authenticatorMock, times(1)).authenticateUser(username, password);
-        verify(authenticatorMock, atLeastOnce()).authenticateUser(username, password);
-        verify(authenticatorMock, atLeast(1)).authenticateUser(username, password);
-        verify(authenticatorMock, atMost(1)).authenticateUser(username, password);
-        //timeout
-        verify(authenticatorMock, timeout(100)).authenticateUser(username, password);
-        //timeout for n method calls
-        verify(authenticatorMock, timeout(100).times(1)).authenticateUser(username, password);
+    public void testAuthenticateMockInjection() {
+        String username = "javacodegeeks";
+        String password = "s4f3 p4ssw0rd";
+        when(this.authenticatorMock.authenticateUser(username, password))
+                .thenReturn(true);
+        boolean actual = this.authenticator.authenticate("javacodegeeks", "s4f3 p4ssw0rd");
+        assertTrue(actual);
     }
 }

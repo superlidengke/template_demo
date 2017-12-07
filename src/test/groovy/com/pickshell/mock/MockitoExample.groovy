eb8495cdcffc16b6d9bdecf5f6a2c7b45dea904d
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-12-07 15:33:25 +0800

    mock,verify and spy

---
+++ src/test/groovy/com/pickshell/mock/MockitoExample.groovy
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class MockitoExample {
+    @Test
+    public void test() {
+        List<String> mockList = mock(List.class);
+        println mockList.add("First");
+        when(mockList.get(0)).thenReturn("Mockito");
+        when(mockList.get(1)).thenReturn("JCG");
+        assertEquals("Mockito", mockList.get(0));
+        assertEquals("JCG", mockList.get(1));
+    }
+    @Test
+    public void verifyInteractions() {
+        Set mockSet = mock(Set.class);
+        Set<String> toAdd = new HashSet<String>();
+        mockSet.addAll(toAdd);
+        mockSet.clear();
+        verify(mockSet).addAll(toAdd);
+        verify(mockSet).clear();
+        //Stub method calls
+        assert mockSet.size() == 0
+        when(mockSet.size()).thenReturn(10);
+        assert mockSet.size() == 10
+    }
+    @Test
+    public void testSpy() {
+        List list = new LinkedList();
+        List spy = spy(list);
+        try {
+            when(spy.get(0)).thenReturn("foo");
+        } catch(IndexOutOfBoundsException e) {
+            // Expected
+            println("can't use when with spy")
+        }
+        doReturn("foo").when(spy).get(0);
+        assert spy.get(0)== 'foo'
+    }
+}
